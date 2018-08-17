a2dd7850d5e62e20e23e92d59e0165ac4cd7763c
NGPerez <nperez@ces.com.uy>
2018-08-17 12:05:07 -0300

    Rector listo2

--- BPM6/Rector.sikuli/Rector.py
+++ BPM6/Rector.sikuli/Rector.py
@@ -3,16 +3,18 @@ os.system("taskkill /f /im iexplore.exe")
 
 f = open('Parametros.txt','r')
 inp = open('Entradas.txt','r')
-l = open('Resultado.txt','w')
+l = open('Log.txt','w')
+S = open ('Resultado.txt','w')
 Nav = f.readline()
 Url = f.readline()
 Nav = 'C:\Program Files\Internet Explorer\iexplore.exe'
 openApp (Nav)
 wait(2)
-wait("1534436384508.png")
+#wait("Home.PNG")
 wait(4)
 click("1534440084048.png")
-paste(Pattern("1534355682379.png").similar(0.60), Url)
+wait(1)
+paste("1534508316424.png", Url)
 wait(3)
 type (Key.ENTER)
 
@@ -47,36 +49,48 @@ else:
                 if(exists("Poliza.png")):
                     click("Poliza-1.png")
                     cantMatriculas = inp.readline() 
-                    Matricula = inp.readline()
-                    paste("1534436811441.png", Matricula)
-                    click("1534436556126.png")
-                    wait("1534436610250.png")
-                    m = find("1534439738589.png").left(83)
-                    #l.write(m.getX())
-                    #l.write(m.getY())
-                    #l.write(m.text()) 
-                    m.highlight (3)
-                    fecha = m.text() 
-                    dia = fecha[0:2]
-                    mes = fecha[3:5]
-                    anio = fecha[(len(fecha)-4):(len(fecha))]
-                    DiaR = inp.readline()
-                    MesR = inp.readline()
-                    AnioR = inp.readline()
-                    
-                    l.write(dia+'/'+mes+'/'+anio)   
-                    l.write(anio[3:3]) 
-                    
-                    if (anio[3:3] == 'S'):
-                       anio[3:3] = 5
-                       l.write(anio) 
+                    for x in range(0, int(cantMatriculas)):
+                        Matricula = inp.readline()
+                        DiaR = inp.readline()
+                        MesR = inp.readline()
+                        AnioR = inp.readline()
+                        Serie = inp.readline()
+                        NroD = inp.readline()
+                        paste("1534436811441.png", Matricula)
+                        click("1534436556126.png")
+                        wait("1534436610250.png")
+                        m = find("1534439738589.png").left(83)
+                        m.highlight (3)
+                        fecha = m.text() 
+                        dia = fecha[0:2]
+                        mes = fecha[3:5]
+                        anio = fecha[(len(fecha)-4):(len(fecha))]
+                        #l.write(dia+'/'+mes+'/'+ str(anio) +'\n')   
+                      
+                        if (anio == '201S'):
+                            anio = '2015'
+                           
+                        if ((int(AnioR)>=int(anio))and(int(MesR)>=int(mes))and(int(DiaR)>int(dia))):
+                            m2 = find("1534439738589.png").left(121).below(13)
+                            m2.highlight (3)
+                            fechaA = m2.text()
+                            DiaA = fechaA[1:3]
+                            MesA = fechaA[4:6]
+                            AnioA = fechaA[7:11]
+                            l.write(DiaA+'/'+MesA+'/'+ AnioA +'\n')
+                            m3=find("1534514242355.png").below(17)
+                            m3.highlight (3)
+                            dato = m3.text()                            
+                            if ( (int(AnioA)>int(AnioR)) or ( (int(AnioA)==int(AnioR)) and (int(MesA)>int(MesR)) ) or ( (int(AnioA)==int(AnioR)) and (int(MesA)==int(MesR)) and (int(DiaA)>int(DiaR)))):
+                                S.write(Matricula+DiaR+MesR+AnioR+Serie+NroD+dato+'\n')
+                                l.write(Matricula + 'Correcta'+'\n')
+                            else:
+                                l.write(Matricula + 'Incorrecta: Fecha hasta inferior'+'\n')
+                        else:
+                            l.write(Matricula + 'Incorrecta: Fecha desde superior'+'\n')
+                        extra = inp.readline()
+                        click("1534515934104.png")
                         
-                    if ((int(AnioR)>=int(anio))and(int(MesR)>=int(mes))and(int(DiaR)>int(dia))):
-                        ##Este funciona
-                        l.write('EsteFunciona')
-                    else:
-                        #No funciona debo buscar otro
-                        l.write('Nofunciono')  
                 else:
                     l.write('>> ERROR_ Falta la opción de Consultas/Varias/Póliza por matrícula.' + '\n')
             else:
@@ -84,7 +98,4 @@ else:
         else:
             l.write('>> ERROR_ Falta la opción de Consultas.' + '\n')    
         
-            
-                      
-
 #os.system("taskkill /f /im iexplore.exe")
